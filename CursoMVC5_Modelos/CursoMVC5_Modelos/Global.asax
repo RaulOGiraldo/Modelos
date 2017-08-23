ebd6463a8c3a49634f361cc7d4b3a0d02a4878d8
Desarrollo <Desarrollo@DESKTOP-LD7K74V>
2017-08-22 22:32:50 -0500

    Proyecto Inicial de Personas

---
+++ CursoMVC5_Modelos/CursoMVC5_Modelos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CursoMVC5_Modelos.MvcApplication" Language="C#" %>
---
+++ CursoMVC5_Modelos/CursoMVC5_Modelos/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC5_Modelos.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string CodigoPostal { get; set; }
+    }
+}
