2a5cc4c0e243706734d3ae6f470f0b12fdd79906
Desarrollo <Desarrollo@DESKTOP-LD7K74V>
2017-08-23 22:18:40 -0500

    Se modifico el Model Persona para colocar el required y la longitud

---
+++ CursoMVC5_Modelos/CursoMVC5_Modelos/Migrations/201708230315536_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace CursoMVC5_Modelos.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Personas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        CodigoPostal = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Personas");
+        }
+    }
+}
--- CursoMVC5_Modelos/CursoMVC5_Modelos/Models/Persona.cs
+++ CursoMVC5_Modelos/CursoMVC5_Modelos/Models/Persona.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace CursoMVC5_Modelos.Models
     public class Persona
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Este campo {0} es Obligatorio ...")]
         public string Nombre { get; set; }
+        [StringLength(6, MinimumLength = 5, ErrorMessage = "El campo {0} debe tener una longitud minima de {2} y una longitud máxima de {1}")]
         public string CodigoPostal { get; set; }
     }
 }
